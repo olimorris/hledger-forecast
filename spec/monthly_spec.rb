9028329857a554459ce0e56006c5d7da217aa036
Oli Morris <olimorris@users.noreply.github.com>
2023-04-14 19:49:28 +0100

    tests: fix

--- spec/monthly_spec.rb
+++ spec/monthly_spec.rb
@@ -2,35 +2,32 @@ require_relative '../lib/hledger_forecast'
 
 RSpec.describe 'generate' do
   it 'generates a forecast with correct MONTHLY transactions' do
-    transactions = File.read(File.join(__dir__, 'stubs/transactions.journal'))
-    forecast = File.read(File.join(__dir__, 'stubs/monthly/forecast_monthly.yml'))
+    transactions = File.read('spec/stubs/transactions.journal')
+    forecast = File.read('spec/stubs/monthly/forecast_monthly.yml')
 
-    generated_journal = HledgerForecast::Generator.create_journal_entries(transactions, forecast, '2023-03-01',
-                                                                          '2023-05-30')
+    generated_journal = HledgerForecast::Generator.create_journal_entries(transactions, forecast, '2023-03-01', '2023-05-30')
 
-    expected_output = File.read(File.join(__dir__, 'stubs/monthly/output_monthly.journal'))
+    expected_output = File.read('spec/stubs/monthly/output_monthly.journal')
     expect(generated_journal).to eq(expected_output)
   end
 
   it 'generates a forecast with correct MONTHLY transactions that have an end date' do
-    transactions = File.read(File.join(__dir__, 'stubs/transactions.journal'))
-    forecast = File.read(File.join(__dir__, 'stubs/monthly/forecast_monthly_enddate.yml'))
+    transactions = File.read('spec/stubs/transactions.journal')
+    forecast = File.read('spec/stubs/monthly/forecast_monthly_enddate.yml')
 
-    generated_journal = HledgerForecast::Generator.create_journal_entries(transactions, forecast, '2023-03-01',
-                                                                          '2023-08-30')
+    generated_journal = HledgerForecast::Generator.create_journal_entries(transactions, forecast, '2023-03-01', '2023-08-30')
 
-    expected_output = File.read(File.join(__dir__, 'stubs/monthly/output_monthly_enddate.journal'))
+    expected_output = File.read('spec/stubs/monthly/output_monthly_enddate.journal')
     expect(generated_journal).to eq(expected_output)
   end
 
   it 'generates a forecast with correct MONTHLY transactions that have an end date, at the top level' do
-    transactions = File.read(File.join(__dir__, 'stubs/transactions.journal'))
-    forecast = File.read(File.join(__dir__, 'stubs/monthly/forecast_monthly_enddate_top.yml'))
+    transactions = File.read('spec/stubs/transactions.journal')
+    forecast = File.read('spec/stubs/monthly/forecast_monthly_enddate_top.yml')
 
-    generated_journal = HledgerForecast::Generator.create_journal_entries(transactions, forecast, '2023-03-01',
-                                                                          '2023-08-30')
+    generated_journal = HledgerForecast::Generator.create_journal_entries(transactions, forecast, '2023-03-01', '2023-08-30')
 
-    expected_output = File.read(File.join(__dir__, 'stubs/monthly/output_monthly_enddate_top.journal'))
+    expected_output = File.read('spec/stubs/monthly/output_monthly_enddate_top.journal')
     expect(generated_journal).to eq(expected_output)
   end
 end
