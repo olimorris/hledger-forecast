a71307e0027995f78e7be690152120b8683b05ad
Oli Morris <olimorris@users.noreply.github.com>
2023-04-22 23:51:47 +0100

    fix: tests

--- lib/hledger_forecast.rb
+++ lib/hledger_forecast.rb
@@ -14,5 +14,5 @@ Money.rounding_mode = BigDecimal::ROUND_HALF_UP
 require_relative 'hledger_forecast/version'
 require_relative 'hledger_forecast/generator'
 require_relative 'hledger_forecast/summarize'
-require_relative 'hledger_forecast/checker'
+# require_relative 'hledger_forecast/checker'
 require_relative 'hledger_forecast/cli'
