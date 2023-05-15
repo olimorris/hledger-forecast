ab96239515f903112b25a7a38022e3c6e1b74083
Oli Morris <olimorris@users.noreply.github.com>
2023-05-15 22:44:08 +0100

    update tests

--- lib/hledger_forecast/summarizer.rb
+++ lib/hledger_forecast/summarizer.rb
@@ -2,9 +2,6 @@ module HledgerForecast
   # Summarise a forecast YAML file and output it to the CLI
   # TODO: Rename this to Summarizer and the main method becomes summarize
   class Summarizer
-    @table = nil
-    @generator = nil
-
     def self.summarize(config, cli_options)
       new.summarize(config, cli_options)
     end
@@ -12,6 +9,7 @@ module HledgerForecast
     def summarize(config, cli_options = nil)
       @forecast = YAML.safe_load(config)
       @settings = Settings.config(@forecast, cli_options)
+      @table = Terminal::Table.new
 
       generate(@forecast)
     end
@@ -19,28 +17,24 @@ module HledgerForecast
     private
 
     def generate(forecast)
-      table = init_table
+      init_table
 
       category_totals = {}
       %w[monthly quarterly half-yearly yearly once custom].each do |period|
         category_totals[period] = sum_transactions(forecast, period)
       end
 
-      add_categories_to_table(table, category_totals, forecast)
+      add_categories_to_table(category_totals, forecast)
 
-      table.add_separator
-      format_total(table, "TOTAL", category_totals.values.map(&:values).flatten.sum)
+      @table.add_separator
+      format_total("TOTAL", category_totals.values.map(&:values).flatten.sum)
 
-      puts table
+      @table
     end
 
     def init_table
-      table = Terminal::Table.new
-
-      table.add_row([{ value: 'FORECAST SUMMARY'.bold, colspan: 3, alignment: :center }])
-      table.add_separator
-
-      table
+      @table.add_row([{ value: 'FORECAST SUMMARY'.bold, colspan: 3, alignment: :center }])
+      @table.add_separator
     end
 
     def sum_transactions(forecast, period)
@@ -79,10 +73,10 @@ module HledgerForecast
       amount.to_f < 0 ? formatted_amount.green : formatted_amount.red
     end
 
-    def add_rows_to_table(table, row_data, period_total, custom: false)
+    def add_rows_to_table(row_data, period_total, custom: false)
       if custom
         row_data[:periods].each do |period|
-          table.add_row [{ value: period[:category], alignment: :left },
+          @table.add_row [{ value: period[:category], alignment: :left },
                           { value: period[:frequency], alignment: :right },
                           { value: format_amount(period[:amount]), alignment: :right }]
 
@@ -90,7 +84,7 @@ module HledgerForecast
         end
       else
         row_data.each do |category, amount|
-          table.add_row [{ value: category, colspan: 2, alignment: :left },
+          @table.add_row [{ value: category, colspan: 2, alignment: :left },
                           { value: format_amount(amount), alignment: :right }]
 
           period_total += amount
@@ -100,7 +94,7 @@ module HledgerForecast
       period_total
     end
 
-    def add_categories_to_table(table, categories, forecast_data)
+    def add_categories_to_table(categories, forecast_data)
       first_period = true
       categories.each do |period, total|
         category_total = total.reject { |_, amount| amount == 0 }
@@ -108,21 +102,19 @@ module HledgerForecast
 
         sorted_category_total = sort_transactions(category_total)
 
-        table.add_separator unless first_period
-        table.add_row([{ value: period.capitalize.bold, colspan: 3, alignment: :center }])
+        @table.add_separator unless first_period
+        @table.add_row([{ value: period.capitalize.bold, colspan: 3, alignment: :center }])
 
         period_total = 0
         period_total += if period == 'custom'
-                          add_rows_to_table(table, sum_custom_transactions(forecast_data), period_total, custom: true)
+                          add_rows_to_table(sum_custom_transactions(forecast_data), period_total, custom: true)
                         else
-                          add_rows_to_table(table, sorted_category_total, period_total)
+                          add_rows_to_table(sorted_category_total, period_total)
                         end
 
-        format_total(table, "#{period.capitalize} TOTAL", period_total)
+        format_total("#{period.capitalize} TOTAL", period_total)
         first_period = false
       end
-
-      table
     end
 
     def sort_transactions(category_total)
@@ -132,9 +124,9 @@ module HledgerForecast
       negatives.concat(positives).to_h
     end
 
-    def format_total(table, text, total)
-      table.add_row [{ value: text.bold, colspan: 2, alignment: :left },
-                      { value: format_amount(total).bold, alignment: :right }]
+    def format_total(text, total)
+      @table.add_row [{ value: text.bold, colspan: 2, alignment: :left },
+                     { value: format_amount(total).bold, alignment: :right }]
     end
   end
 end
